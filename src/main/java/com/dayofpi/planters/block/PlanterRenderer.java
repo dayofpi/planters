120929ba9f33317f530acc239a0427c499032af0
dayofpi <thedayofpi@gmail.com>
2023-07-03 21:06:06 -0300

    Updated texture & fixed tall plant rendering

--- src/main/java/com/dayofpi/planters/block/PlanterRenderer.java
+++ src/main/java/com/dayofpi/planters/block/PlanterRenderer.java
@@ -8,15 +8,24 @@ import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DoublePlantBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
 
 public class PlanterRenderer implements BlockEntityRenderer<PlanterBlockEntity> {
     private final BlockRenderDispatcher blockRenderer;
 
     public PlanterRenderer(BlockEntityRendererProvider.Context context) {
         this.blockRenderer = context.getBlockRenderDispatcher();
+    }
+
+    private BlockState getState(Block block) {
+        if (block instanceof DoublePlantBlock)
+            return block.defaultBlockState().setValue(DoublePlantBlock.HALF, DoubleBlockHalf.UPPER);
+        return block.defaultBlockState();
     }
 
     @Override
@@ -28,13 +37,13 @@ public class PlanterRenderer implements BlockEntityRenderer<PlanterBlockEntity> 
         BlockState blockState3;
 
         if (pBlockEntity.getPlant(PlanterBlock.Slot.RIGHT).getItem() instanceof BlockItem blockItem) {
-            blockState1 = blockItem.getBlock().defaultBlockState();
+            blockState1 = getState(blockItem.getBlock());
         } else blockState1 = Blocks.AIR.defaultBlockState();
         if (pBlockEntity.getPlant(PlanterBlock.Slot.MIDDLE).getItem() instanceof BlockItem blockItem) {
-            blockState2 = blockItem.getBlock().defaultBlockState();
+            blockState2 = getState(blockItem.getBlock());
         } else blockState2 = Blocks.AIR.defaultBlockState();
         if (pBlockEntity.getPlant(PlanterBlock.Slot.LEFT).getItem() instanceof BlockItem blockItem) {
-            blockState3 = blockItem.getBlock().defaultBlockState();
+            blockState3 = getState(blockItem.getBlock());
         } else blockState3 = Blocks.AIR.defaultBlockState();
 
         float f = pBlockEntity.getBlockState().getValue(BlockStateProperties.HORIZONTAL_FACING).getClockWise().toYRot();
